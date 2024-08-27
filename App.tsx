01b47a507cdf0f5eb39d676509fc455a3f8de71f
Jell <mystral7@naver.com>
2024-08-27 20:32:35 +0900

    [Animation] add interval animation(don't use it)

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
 function App(): React.JSX.Element {
-  return <></>;
+  const [y, setY] = useState(0);
+  const moveUp = () => {
+    setInterval(() => setY(prev => prev + 1), 500);
+  };
+  return (
+    <Container>
+      <Box
+        onPress={moveUp}
+        style={{
+          transform: [{translateY: y}],
+        }}
+      />
+    </Container>
+  );
 }
 
 export default App;
