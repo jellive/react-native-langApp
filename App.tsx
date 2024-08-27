ec9190cb6e78434ebc8c3ac6e7be2b1a8287ca1b
Jell <mystral7@naver.com>
2024-08-27 20:43:08 +0900

    [Animation] add animated value, animated box

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Animated, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -6,24 +7,27 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `;
-
+// Animated는 무조건 Animated.Value로 써야 함. Animated API와 함께 함.
+// animated value는 절대 직접적으로 수정하면 안된다.
+// animated compoment는 image, scrollview, text, view, flatlist, sectionlist 로만 기본으로 되며, 다른 것은 createAnimatedComponen로 만들어야함.
 const Box = styled.TouchableOpacity`
   background-color: tomato;
   width: 200px;
   height: 200px;
 `;
 
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
 function App(): React.JSX.Element {
-  const [y, setY] = useState(0);
-  const moveUp = () => {
-    setInterval(() => setY(prev => prev + 1), 500);
-  };
+  const Y = new Animated.Value(0);
+
+  const moveUp = () => {};
   return (
     <Container>
-      <Box
+      <AnimatedBox
         onPress={moveUp}
         style={{
-          transform: [{translateY: y}],
+          transform: [{translateY: Y}],
         }}
       />
     </Container>
