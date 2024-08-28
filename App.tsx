38001f809da56e9709d7c0b46729c7e24445c442
Jell <mystral7@naver.com>
2024-08-28 20:50:56 +0900

    [Prebuild] add expo

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, {useRef, useState} from 'react';
-import {Animated, Dimensions, PanResponder} from 'react-native';
+import {Animated, Dimensions, PanResponder, View} from 'react-native';
 import styled from 'styled-components/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Container = styled.View`
   flex: 1;
@@ -9,8 +10,18 @@ const Container = styled.View`
   background-color: #00a8ff;
 `;
 
+const Card = styled.View``;
+
+const AnimatedCard = Animated.createAnimatedComponent(Card);
+
 function App(): React.JSX.Element {
-  return <Container></Container>;
+  return (
+    <Container>
+      <AnimatedCard>
+        <Ionicons name="pizza" color="#192a56" />
+      </AnimatedCard>
+    </Container>
+  );
 }
 
 export default App;
