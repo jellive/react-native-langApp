7e2338823a1c1b34ff270b0e74f8afff736896b8
Jell <mystral7@naver.com>
2024-08-28 21:39:02 +0900

    [Card] add drag animation

--- App.tsx
+++ App.tsx
@@ -27,6 +27,7 @@ function App(): React.JSX.Element {
   const rotation = position.interpolate({
     inputRange: [-200, 200],
     outputRange: ['-15deg', '15deg'],
+    // extrapolate: 'clamp', // -200, 200와 같은 inputRange를 넘어설때 추가로 해줄 수 설정할 수 있는(무시하거나 계속하거나) 옵션. clamp는 제시한 최소, 최대값을 넘지 않는다. extend는 넘어서도 진행함.
   });
 
   // Animations
@@ -50,8 +51,23 @@ function App(): React.JSX.Element {
         position.setValue(dx);
       },
       onPanResponderGrant: () => onPressIn.start,
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goCenter]).start();
+      onPanResponderRelease: (_, {dx}) => {
+        console.log('release', dx);
+        if (dx < -200) {
+          console.log('dismiss to the left');
+          Animated.spring(position, {
+            toValue: -400,
+            useNativeDriver: true,
+          }).start();
+        } else if (dx > 200) {
+          console.log('dismiss to the right');
+          Animated.spring(position, {
+            toValue: 400,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     }),
   ).current; // current 중요하다!
