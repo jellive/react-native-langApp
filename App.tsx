32e92dce93410684c14103a4b6ef7dc3aad44bdf
Jell <mystral7@naver.com>
2024-08-29 19:12:23 +0900

    [DND] add complex animation (eat word in btn)

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {Animated, PanResponder, Text, View} from 'react-native';
+import {Animated, Easing, PanResponder, Text, View} from 'react-native';
 import styled from 'styled-components/native';
 import {Ionicons} from '@expo/vector-icons';
 import icons from './icons';
@@ -36,6 +36,7 @@ const Center = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
+  z-index: 10;
 `;
 
 const IconCard = styled(Animated.createAnimatedComponent(View))`
@@ -46,8 +47,20 @@ const IconCard = styled(Animated.createAnimatedComponent(View))`
 
 export default function App() {
   // Values
+  const opacity = useRef(new Animated.Value(1)).current;
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
+
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [2, 1],
+    extrapolate: 'clamp', // 안하면 처음에 벗어나서 작아보임.
+  });
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 2],
+    extrapolate: 'clamp',
+  });
   // Animations
 
   const onPressIn = Animated.spring(scale, {
@@ -62,6 +75,17 @@ export default function App() {
     toValue: 0,
     useNativeDriver: true,
   });
+  const onDropScale = Animated.timing(scale, {
+    toValue: 0,
+    duration: 50,
+    useNativeDriver: true,
+  });
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    duration: 50,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
   // Pan Responders
   const panResponder = useRef(
     PanResponder.create({
@@ -72,8 +96,26 @@ export default function App() {
       onPanResponderGrant: () => {
         onPressIn.start();
       },
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goHome]).start();
+      onPanResponderRelease: (_, {dy}) => {
+        if (dy < -200 || dy > 200) {
+          // drop
+
+          Animated.sequence([
+            Animated.parallel([onDropScale, onDropOpacity]),
+            Animated.timing(position, {
+              toValue: 0,
+              duration: 50,
+              easing: Easing.linear,
+              useNativeDriver: true,
+            }),
+          ]).start();
+
+          // position.setValue({x: 0, y: 0});
+          // Animated.timing(position, {
+          //   toValue: 0,
+          //   useNativeDriver: true,
+          // }).start();
+        } else Animated.parallel([onPressOut, goHome]).start();
 
         // Animated.sequence([onPressOut, goHome]).start(); // sequence는 동시실행이 아닌 순차실행임
       },
@@ -85,7 +127,7 @@ export default function App() {
   return (
     <Container>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{transform: [{scale: scaleOne}]}}>
           <Word color={GREEN}>알아</Word>
         </WordContainer>
       </Edge>
@@ -93,13 +135,14 @@ export default function App() {
         <IconCard
           {...panResponder.panHandlers}
           style={{
+            opacity,
             transform: [...position.getTranslateTransform(), {scale}],
           }}>
           <Ionicons name="beer" color={GREY} size={66} />
         </IconCard>
       </Center>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{transform: [{scale: scaleTwo}]}}>
           <Word color={RED}>몰라</Word>
         </WordContainer>
       </Edge>
