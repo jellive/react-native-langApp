841f91e83e29acb9f1b4dc23b9efe74f6a501f1d
Jell <mystral7@naver.com>
2024-08-29 18:46:46 +0900

    [DND] create layout

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import {Animated, PanResponder, Text, View} from 'react-native';
 import styled from 'styled-components/native';
 import {Ionicons} from '@expo/vector-icons';
 import icons from './icons';
+import React, {useRef} from 'react';
 
 const BLACK_COLOR = '#1e272e';
 const GREY = '#485460';
@@ -13,14 +14,66 @@ const Container = styled.View`
   background-color: ${BLACK_COLOR};
 `;
 
+const Edge = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
+`;
+const Word = styled.Text<{color: string}>`
+  font-size: 38px;
+  font-weight: 500;
+  color: ${props => props.color};
+`;
+const Center = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 5px 10px;
+  border-radius: 10px;
+`;
+
 export default function App() {
   // Values
-
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
   // Animations
 
   // Pan Responders
 
   // State
 
-  return <></>;
+  return (
+    <Container>
+      <Edge>
+        <WordContainer>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+      <Center>
+        <IconCard
+          style={{
+            transform: [...position.getTranslateTransform(), {scale}],
+          }}>
+          <Ionicons name="beer" color={GREY} size={66} />
+        </IconCard>
+      </Center>
+      <Edge>
+        <WordContainer>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
+    </Container>
+  );
 }
