b2d3ccf8b1d2644a1d89348e9482ccc3419f3621
Jell <mystral7@naver.com>
2024-08-29 17:37:28 +0900

    [Card] add behind card

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {Animated, Dimensions, PanResponder, View} from 'react-native';
 import styled from 'styled-components/native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+type IoniconsIconNames = keyof typeof Ionicons;
 const Container = styled.View`
   flex: 1;
   justify-content: center;
@@ -18,6 +19,8 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   align-items: center;
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+  position: absolute;
+  /* 카드를 겹치게 두기 위해 absolute를 넣음 */
 `;
 
 const Btn = styled.TouchableOpacity`
@@ -25,7 +28,13 @@ const Btn = styled.TouchableOpacity`
 `;
 const BtnContainer = styled.View`
   flex-direction: row;
-  margin-top: 100px;
+  flex: 1;
+`;
+
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App(): React.JSX.Element {
@@ -37,6 +46,13 @@ function App(): React.JSX.Element {
     outputRange: ['-15deg', '15deg'],
     // extrapolate: 'clamp', // -200, 200와 같은 inputRange를 넘어설때 추가로 해줄 수 설정할 수 있는(무시하거나 계속하거나) 옵션. clamp는 제시한 최소, 최대값을 넘지 않는다. extend는 넘어서도 진행함.
   });
+
+  const secondScale = position.interpolate({
+    inputRange: [-250, 0, 250],
+    outputRange: [1, 0.7, 1],
+    extrapolate: 'clamp',
+  });
+  // 첫 카드를 밀면 뒤에서 천천히 커지는 효과를 가져옴.
 
   // Animations
   const onPressIn = Animated.spring(scale, {
@@ -94,13 +110,18 @@ function App(): React.JSX.Element {
 
   return (
     <Container>
-      <Card
-        {...panResponder.panHandlers}
-        style={{
-          transform: [{scale}, {translateX: position}, {rotateZ: rotation}],
-        }}>
-        <Ionicons name="pizza" color="#192a56" size={98} />
-      </Card>
+      <CardContainer>
+        <Card style={{transform: [{scale: secondScale}]}}>
+          <Ionicons name={'beer'} color="#192a56" size={98} />
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={{
+            transform: [{scale}, {translateX: position}, {rotateZ: rotation}],
+          }}>
+          <Ionicons name="pizza" color="#192a56" size={98} />
+        </Card>
+      </CardContainer>
       <BtnContainer>
         <Btn onPress={closePress}>
           <Ionicons name="close-circle" color="white" size={58} />
